b76400798a3521c7d157ea0a7df81fb9f10d0ca9
Eliot <saltycrane@gmail.com>
2016-03-14 17:56:25 -0700

    dont separate vendor js because jquery was being excluded

--- L2.methodB.react.router/webpack.config.js
+++ L2.methodB.react.router/webpack.config.js
@@ -3,7 +3,6 @@ var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var merge = require('webpack-merge');
 var path = require('path');
-var pkg = require('./package.json');
 var webpack = require('webpack');
 
 var TARGET = process.env.npm_lifecycle_event;
@@ -67,10 +66,9 @@ if (TARGET === 'start' || !TARGET) {
 else if (TARGET === 'build') {
   module.exports = merge(common, {
     devtool: 'source-map',
-    entry: {
-      app: APP_PATH,
-      vendor: Object.keys(pkg.dependencies)
-    },
+    entry: [
+      APP_PATH
+    ],
     output: {
       path: BUILD_PATH,
       filename: '[name].[chunkhash].js?'
@@ -90,11 +88,7 @@ else if (TARGET === 'build') {
         compress: {
           warnings: false
         }
-      }),
-      new webpack.optimize.CommonsChunkPlugin(
-        'vendor',
-        '[name].[chunkhash].js'
-      )
+      })
     ],
     module: {
       loaders: [
